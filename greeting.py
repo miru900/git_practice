6edcbd33a627b36017e883a22337c4dba387f1a0
miru900 <miru405@gmail.com>
2026-09-23 16:05:19 +0900

    Add greeting script

---
+++ greeting.py
@@ -0,0 +1,10 @@
+"""Print a friendly greeting."""
+
+
+def greet(name: str = "world") -> str:
+    """Return a greeting for *name*."""
+    return f"Hello, {name}!"
+
+
+if __name__ == "__main__":
+    print(greet("Git"))
